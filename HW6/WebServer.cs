c2b690a353ee568fb1bfa3816f512690c372d246
Thom Hemenway <hemenwayarts@gmail.com>
2016-10-14 16:18:29 -0700

    HW6 is now HW7

--- HW6/WebServer.cs
+++ HW6/WebServer.cs
@@ -10,6 +10,10 @@ namespace CS422
 {
 	public class WebServer
 	{
+		const int SECOND = 1000;
+		const int FIRST_THRESHOLD = 2048;
+		const int SECOND_THRESHOLD = (100 * 1024);
+
         static List<WebService> services = new List<WebService>();
         static ThreadPool threadPool = new ThreadPool();
 
@@ -81,22 +85,53 @@ namespace CS422
             WebRequest req = null;
             NetworkStream stream = client.GetStream();
             StringBuilder request = new StringBuilder();
-            int buffer;
-            int part = 0;
+			int buffer, part = 0;
+			long bytesRead = 0;
             // Important stuff for the WebRequest
             string method = "", uri = "", httpversion = "";
             Dictionary<string, string> headers = new Dictionary<string, string>();
 
+
+			// Start the read timeout to timeout in 2 seconds
+			System.Timers.Timer readTimeout = new System.Timers.Timer(2 * SECOND);
+			readTimeout.AutoReset = false;
+			readTimeout.Elapsed += (o, e) => {
+				client.Close();
+				client.GetStream().Close();
+			};
+
+			// Start the request timeout to timeout in 10 seconds
+			System.Timers.Timer requestTimeout = new System.Timers.Timer(10 * SECOND);
+			requestTimeout.AutoReset = false;
+			requestTimeout.Elapsed += (o, e) =>
+			{
+				client.Close();
+				client.GetStream().Close();
+			};;
+
+			// Start the timers
+			readTimeout.Enabled = true;
+			requestTimeout.Enabled = true;
+
             // Read byte by byte
             while ((buffer = stream.ReadByte()) >= 0)
             {
+				// Stop the read timeout
+				readTimeout.Stop();
+
+				// Our first data threshold
+				if (part == 0 && ++bytesRead >= FIRST_THRESHOLD)
+				{
+					client.Close();
+					return null;
+				}
+
                 char letter = Encoding.ASCII.GetChars(new byte[] { (byte)buffer })[0];
                 request.Append(letter);
 
                 // Read line by line
                 if (letter == '\n')
-                {
-
+				{
                     switch (part)
                     {
                         // METHOD URL HTTPVERSION
@@ -126,12 +161,23 @@ namespace CS422
                             // We have reached the end of the headers, body is starting
                             if (seperator == -1)
                             {
+								// Stop the request timer
+								requestTimeout.Stop();
+
                                 // Save the body and make WebRequest
-                                req = new WebRequest(ref stream, method, uri, httpversion, headers, new ConcatStream(new MemoryStream(), stream));
+                                req = new WebRequest(
+									ref stream, method, uri, httpversion, headers, new ConcatStream(new MemoryStream(), stream));
                                 return req;
                             }
                             else
                             {// Get headers
+							 // Second data threshold
+								if (bytesRead >= SECOND_THRESHOLD)
+								{
+									client.Close();
+									return null;
+								}
+
                                 string name = line.Substring(0, seperator);
                                 string value = line.Substring(seperator);
 
