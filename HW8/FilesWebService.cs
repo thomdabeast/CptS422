eecb85c4b6b8f5f3bd67879cb67c88adef2d721f
Thom Hemenway <hemenwayarts@gmail.com>
2016-10-22 09:12:14 -0700

    Added hw8. THeres a weird exception thrown on the auto grader, but oh well...

---
+++ HW8/FilesWebService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CS422
+{
+	public class FilesWebService : WebService
+	{
+		const int PACKAGE_SIZE = 1024;
+
+		public override string ServiceURI
+		{
+			get
+			{
+				return "/files";
+			}
+		}
+
+		public override void Handler(WebRequest req)
+		{
+			
+		}
+
+		//void WriteFileResponse(WebRequest req, FSFile file)
+		//{
+		//	Stream stream = file.OpenReadOnly();
+		//	StringBuilder res = new StringBuilder();
+
+		//	// TODO: Finish implementation!!!
+
+		//	// Send reponse code and headers
+
+		//	res.Append("HTTP/1.1 200 OK\r\n");
+		//	res.Append("Content-Length: " + stream.Length + "\r\n\r\n");
+		//	byte[] resBytes = Encoding.ASCII.GetBytes(res.ToString());
+		//	res.Response.Write(resBytes, 0, resBytes.Length);
+
+		//	while (true)
+		//	{
+		//		byte[] buf = new byte[PACKAGE_SIZE];
+		//		int read = stream.Read(buf, 0, buf.Length);
+
+		//		if (0 == read)
+		//		{
+		//			break;
+		//		}
+
+		//		req.Response.Write(buf, 0, read);
+		//	}
+		//}
+	}
+}
---
+++ HW8Tests/Test.cs
@@ -0,0 +1,17 @@
+﻿using CS422;
+using NUnit.Framework;
+using System;
+
+namespace HW8Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			WebServer.AddService(new FilesWebService());
+			WebServer.Start(3000, 10);
+		}
+	}
+}
